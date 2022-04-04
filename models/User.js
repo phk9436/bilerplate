cb3bebefd47b86a449c27953c96f7559139e5a90
digitalworks_hgpark <phk9436phk@gmail.com>
2022-04-04 15:30:50 +0900

    비밀번호암호화

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //salt의 글자수
 
 const userSchema = Schema({
     name : {
@@ -8,8 +10,8 @@ const userSchema = Schema({
     },
     email: {
         type: String,
-        trim: true,
-        unique: 1
+        trim: true
+        //unique: 1
     },
     password: {
         type: String,
@@ -30,6 +32,22 @@ const userSchema = Schema({
     tokenExp: { //토큰 유효기간
         type: Number
     }
+});
+
+userSchema.pre('save', function(next){
+    var {password} = this;
+    //비밀번호를 암호화 시킨다
+    this.isModified('password')?
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+            bcrypt.hash(password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err);
+                password = hash;
+                next();
+            });
+        })
+    : next();
 });
 
 const User = mongoose.model('User', userSchema);
