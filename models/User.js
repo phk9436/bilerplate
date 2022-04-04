89b5a4a0dd11a1f65749220dd03b2e727c24d0ba
digitalworks_hgpark <phk9436phk@gmail.com>
2022-04-04 10:15:46 +0900

    allsetting

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = Schema({
+    name : {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength:5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: { //유효성검사
+        type: String
+    },
+    tokenExp: { //토큰 유효기간
+        type: Number
+    }
+});
+
+export const User = mongoose.model('User', userSchema);
+
+//module.exports = {User}; //모델을 다른곳에 쓸수있도록 export
