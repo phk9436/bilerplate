7fa1b26d53ef8824a35788f06fa4af59e151ba2a
digitalworks_hgpark <phk9436phk@gmail.com>
2022-04-18 10:53:25 +0900

    리듀서 1

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,9 @@
-import React,{useEffect} from 'react';
-import axios from 'axios';
+import React from 'react'
 
-function LandingPage(){
-    useEffect(()=> {
-        axios.get('/api/hello').then((response) => {console.log(response.data)});
-    }, []);
-
-    return (
-        <div>
-            LandingPage
-        </div>
-    )
+function LandingPage() {
+  return (
+    <div>LandingPage</div>
+  )
 }
 
-export default LandingPage;
+export default LandingPage
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css';
+//import 'antd/dist/antd.css';
+import {Provider} from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'; // './_reducers/index.js'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.createRoot(
   document.getElementById("root"),
 )
 .render(
+  <Provider
+    store={createStoreWithMiddleware(Reducer, 
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__())}
+  >
     <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
