afb59d87eb50a49518a61d2550356d5f7e1e4d12
digitalworks_hgpark <phk9436phk@gmail.com>
2022-04-21 10:59:44 +0900

    리액트 코딩앙마

---
+++ project-angma/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+
+function App() {
+  const name = "Tom";
+  const naver = {
+    name: "네이버",
+    url: "https://naver.com"
+  }
+  return (
+    <div className="App">
+      <Welcome/>
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
