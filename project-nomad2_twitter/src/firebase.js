ffa86e8397ec450ab66e1cbde461ffa09a0c098d
digitalworks_hgpark <phk9436phk@gmail.com>
2022-06-27 10:43:36 +0900

    recoil사용

--- project-nomad2_twitter/src/firebase.js
+++ project-nomad2_twitter/src/firebase.js
@@ -1,18 +1,18 @@
 import * as firebase from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
-import {getStorage} from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import "firebase/compat/storage";
 
-const {initializeApp} = firebase;
+const { initializeApp } = firebase;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzeeOfcFYelfHjtmnawqaNmsbksQA41vk",
-    authDomain: "nwitter-41dcf.firebaseapp.com",
-    projectId: "nwitter-41dcf",
-    storageBucket: "nwitter-41dcf.appspot.com",
-    messagingSenderId: "904151530048",
-    appId: "1:904151530048:web:aa13c64082b0fa51eafda5"
-  };
+  apiKey: "AIzaSyBzeeOfcFYelfHjtmnawqaNmsbksQA41vk",
+  authDomain: "nwitter-41dcf.firebaseapp.com",
+  projectId: "nwitter-41dcf",
+  storageBucket: "nwitter-41dcf.appspot.com",
+  messagingSenderId: "904151530048",
+  appId: "1:904151530048:web:aa13c64082b0fa51eafda5",
+};
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const dbService = getFirestore();
